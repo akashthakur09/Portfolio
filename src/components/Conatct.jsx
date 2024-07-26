a69b91f1ce31ee4dd853d26da3f32a2fad891f14
Akash Thakur <akash0218.nitt@gmail.com>
2024-07-26 23:58:54 +0530

    styling

--- src/components/Conatct.jsx
+++ src/components/Conatct.jsx
@@ -28,7 +28,7 @@ const Conatct = () => {
   };
   return (
     <>
-      <svg className='contactSvg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1a1c1f" fill-opacity="1" d="M0,192L80,165.3C160,139,320,85,480,90.7C640,96,800,160,960,176C1120,192,1280,160,1360,144L1440,128L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+      <svg className='contactSvg' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#071f47" fill-opacity="1" d="M0,192L80,165.3C160,139,320,85,480,90.7C640,96,800,160,960,176C1120,192,1280,160,1360,144L1440,128L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
     <div className='contactSection' id="contact">
         <div className='leftContact'>
           <div className='leftInner'>
